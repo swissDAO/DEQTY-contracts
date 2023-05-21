ccf91a5c7f59fcc055b68701949a66ad1497fce1
Lukas Meyer <lukas@ibex.host>
2023-05-21 11:12:12 +0200

    Factory initial.

---
+++ src/DEQTYFactory.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./DEQTYProject.sol";
+
+contract DEQTYDeployer {
+  address[] private deployedProjects;
+  mapping(uint256 => bool) private identifiers;
+
+  event ProjectDeployed(address indexed deployedAddress, address indexed ownerAddress, uint256 identifier);
+
+  function getProjectsLength() external view returns (uint256) {
+    return deployedProjects.length;
+  }
+
+  function getProject(uint256 _index) external view returns (address) {
+    require(_index < deployedProjects.length, "Invalid index");
+    return deployedProjects[_index];
+  }
+
+  function deploy(
+    string calldata _name,
+    string calldata _description,
+    address _owner
+  ) external returns (uint256 _identifier) {
+    _identifier = getIdentifier();
+    require(!identifiers[_identifier], "Identifier already used");
+    identifiers[_identifier] = true;
+
+    DEQTYProject project = new DEQTYProject(
+        _name,
+        _description,
+        _identifier,
+        _owner
+    );
+    deployedProjects.push(address(project));
+
+    emit ProjectDeployed(address(project), msg.sender, _identifier);
+
+    return _identifier;
+  }
+
+  function getIdentifier() private returns (uint256) {
+    uint256 _identifier = 1;
+    while (identifiers[_identifier]) {
+        _identifier = _identifier << 1;
+    }
+    return _identifier;
+  }
+}
---
+++ src/DEQTYProject.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract DEQTYProject {
+
+  constructor(
+    string _name,
+    string _description,
+    uint256 _identifier,
+    address _owner) {
+  }
+}
